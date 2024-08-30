684af6100edf16da4bfeaffb9de842ce3567cdd0
Kevin-Tsh <158796604+Kevin-Tsh@users.noreply.github.com>
2024-08-30 12:39:10 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,15 +11,15 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "237698581946"
-global.ownername = "🐉 Limule Solitarus"
+global.ownernomer = "243891716833"
+global.ownername = "🐉 MR KEVIN TSH"
 global.ytname = "YT: Deaethgun635"
 global.socialm = "GitHub: Limule3650"
 global.location = "India, Rampur, city"
 
-global.ownernumber = '237698581946'  //creator number
-global.ownername = '🐉 Limule Solitarus' //owner name
-global.botname = 'DEATH RAPHAEL-BUG' //name of the bot
+global.ownernumber = '243891716833'  //creator number
+global.ownername = '🐉 MR KEVIN TSH' //owner name
+global.botname = 'TSH_BUG_BOT' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
@@ -38,11 +38,11 @@ global.prefa = ['','!','.','#','&','•']
 //false=disable and true=enable
 global.autoRecording = false //auto recording
 global.autoTyping = false //auto typing
-global.autorecordtype = true //auto typing + recording
+global.autorecordtype = false //auto typing + recording
 global.autoread = false //auto read messages
-global.autobio = true //auto update bio
+global.autobio = false //auto update bio
 global.anti92 = false //auto block +92 
-global.autoswview = true //auto view status/story
+global.autoswview = false //auto view status/story
 
 //menu type 
 //v1 is image menu, 
